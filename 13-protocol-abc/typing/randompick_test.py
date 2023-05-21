e9f08699cf8997171f45e36f8b7b9002f813756d
Huy Mac <huy17101999@gmail.com>
2023-05-21 16:51:54 +0700

    Chapter 13

--- 13-protocol-abc/typing/randompick_test.py
+++ 13-protocol-abc/typing/randompick_test.py
@@ -23,3 +23,29 @@ def test_item_type() -> None:  # <7>
     if TYPE_CHECKING:
         reveal_type(item)  # <8>
     assert isinstance(item, int)
+
+"""
+1
+It’s not necessary to import the static protocol to define a class that implements it. Here I imported RandomPicker only to use it on test_isinstance later.
+
+2
+SimplePicker implements RandomPicker—but it does not subclass it. This is static duck typing in action.
+
+3
+Any is the default return type, so this annotation is not strictly necessary, but it does make it more clear that we are implementing the RandomPicker protocol as defined in Example 13-18.
+
+4
+Don’t forget to add -> None hints to your tests if you want Mypy to look at them.
+
+5
+I added a type hint for the popper variable to show that Mypy understands that SimplePicker is consistent-with.
+
+6
+This test proves that an instance of SimplePicker is also an instance of RandomPicker. This works because of the @runtime_checkable decorator applied to RandomPicker, and because SimplePicker has a pick method as required.
+
+7
+This test invokes the pick method from a SimplePicker, verifies that it returns one of the items given to SimplePicker, and then does static and runtime checks on the returned item.
+
+8
+This line generates a note in the Mypy output.
+"""
--- 13-protocol-abc/typing/randompickload.py
+++ 13-protocol-abc/typing/randompickload.py
@@ -4,3 +4,15 @@ from randompick import RandomPicker
 @runtime_checkable  # <1>
 class LoadableRandomPicker(RandomPicker, Protocol):  # <2>
     def load(self, Iterable) -> None: ...  # <3>
+
+"""
+1
+If you want the derived protocol to be runtime checkable, you must apply the decorator again—its behavior is not inherited.22
+
+2
+Every protocol must explicitly name typing.Protocol as one of its base classes in addition to the protocol we are extending. 
+This is different from the way inheritance works in Python.23
+
+3 Back to “regular” object-oriented programming: we only need to declare the method that is new in this derived 
+protocol. The pick method declaration is inherited from RandomPicker. 
+"""
